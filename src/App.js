c2f164a8fdebafd5d647dd5dccdb795465ff4985
unknown <luzhnyi.igor@gmail.com>
2022-10-29 22:34:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import CategoriesContainer from "./components/categories-container/categories-container";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl:
+        "https://images.unsplash.com/photo-1619385746610-e11b2ea6fc1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODN8fGhhdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl:
+        "https://images.unsplash.com/photo-1551488831-00ddcb6c6bd3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8amFja2V0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl:
+        "https://images.unsplash.com/photo-1595341888016-a392ef81b7de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjZ8fHNuZWFrZXJzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl:
+        "https://images.unsplash.com/photo-1615332859295-aa9dc9f01fbc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl:
+        "https://images.unsplash.com/photo-1508908324153-d1a219719814?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    },
+  ];
+
+  return <CategoriesContainer categories={categories} />;
+};
 
 export default App;
---
+++ src/components/categories-container/categories-container.js
@@ -0,0 +1,14 @@
+import "./categories-container.scss";
+import CategoryItem from "../category-item/category-item";
+
+const CategoriesContainer = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default CategoriesContainer;

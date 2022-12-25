bc0cc61886f0c9ebd9948db5924dc4da54a94dbf
Ihor Luzhnyi <iluzhnyi@supportyourapp.com>
2022-12-25 09:23:00 +0200

    added add/remove item functionality

---
+++ src/routes/checkout/checkout.js
@@ -0,0 +1,33 @@
+import "./checkout.scss";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+
+const Checkout = () => {
+  const { cartItems, addItemToCart, removeItemFromCart } =
+    useContext(CartContext);
+
+  return (
+    <div>
+      <h1>Checkout page</h1>
+      <div>
+        {cartItems.map((cartItem) => {
+          const { id, name, quantity } = cartItem;
+          return (
+            <div key={id}>
+              <h2>{name}</h2>
+              <span>{quantity}</span>
+              <br />
+              <span onClick={() => removeItemFromCart(cartItem)}>
+                decrement
+              </span>
+              <br />
+              <span onClick={() => addItemToCart(cartItem)}>increment</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
